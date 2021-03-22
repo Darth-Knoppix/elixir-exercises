eb764f61e5c4a3d9d7d2aa10e52de0b2d7e9a102
Seth Corker <seth.corker@gmail.com>
2021-03-22 20:47:45 +0000

    guards and attributes

--- factorial.exs
+++ factorial.exs
@@ -1,4 +1,4 @@
 defmodule Factorial do
   def of(0), do: 1
-  def of(n), do: n * of(n-1)
+  def of(n) when is_integer(n) and n > 0, do: n * of(n-1)
 end
--- sum.exs
+++ sum.exs
@@ -1,4 +1,7 @@
 defmodule Sum do
   def of(0), do: 0
   def of(n), do: n + of(n-1)
+
+  @author "Seth"
+  def get_author, do: IO.puts "By #{@author}"
 end
