9b7d07933fb65241ee099fed6a4fb536d548ba0a
Seth Corker <seth.corker@gmail.com>
2022-11-04 11:42:00 +0000

    complete collatz

--- elixir/collatz-conjecture/lib/collatz_conjecture.ex
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -1,4 +1,6 @@
 defmodule CollatzConjecture do
+  require Integer
+
   @doc """
   calc/1 takes an integer and returns the number of steps required to get the
   number to 1 when following the rules:
@@ -6,6 +8,26 @@ defmodule CollatzConjecture do
     - if number is even, divide by 2
   """
   @spec calc(input :: pos_integer()) :: non_neg_integer()
+  def calc(input) when input == 1 do
+    0
+  end
+
   def calc(input) do
+    calc_step(input, 1)
+  end
+
+  @spec calc_step(input :: pos_integer(), iteration :: non_neg_integer()) :: non_neg_integer()
+  def calc_step(input, _iteration) when input == 1 do
+    1
+  end
+
+  def calc_step(input, iteration) do
+    new_num =
+      case Integer.is_odd(input) do
+        true -> 1 + input * 3
+        false -> input / 2
+      end
+
+    calc_step(new_num, iteration + 1)
   end
 end
