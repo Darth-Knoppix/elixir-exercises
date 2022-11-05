9dc303c21cc9038dcd1e649aac42edee37cede6b
Seth Corker <seth.corker@gmail.com>
2022-11-05 20:19:16 +0000

    update

--- elixir/resistor-color-trio/lib/resistor_color_trio.ex
+++ elixir/resistor-color-trio/lib/resistor_color_trio.ex
@@ -1,20 +1,4 @@
 defmodule ResistorColorTrio do
-  @spec code(atom) :: integer()
-  defp code(:black), do: 0
-  defp code(:brown), do: 1
-  defp code(:red), do: 2
-  defp code(:orange), do: 3
-  defp code(:yellow), do: 4
-  defp code(:green), do: 5
-  defp code(:blue), do: 6
-  defp code(:violet), do: 7
-  defp code(:grey), do: 8
-  defp code(:white), do: 9
-
-  @spec convert_with_unit(pos_integer()) :: {pos_integer(), :kiloohms | :ohms}
-  defp convert_with_unit(value) when value > 1000, do: {div(value, 1000), :kiloohms}
-  defp convert_with_unit(value), do: {value, :ohms}
-
   @doc """
   Calculate the resistance value in ohm or kiloohm from resistor colors
   """
@@ -33,4 +17,20 @@ defmodule ResistorColorTrio do
     String.to_integer(Enum.join(num_values, "") <> zero_str)
     |> convert_with_unit()
   end
+
+  @spec code(atom) :: integer()
+  defp code(:black), do: 0
+  defp code(:brown), do: 1
+  defp code(:red), do: 2
+  defp code(:orange), do: 3
+  defp code(:yellow), do: 4
+  defp code(:green), do: 5
+  defp code(:blue), do: 6
+  defp code(:violet), do: 7
+  defp code(:grey), do: 8
+  defp code(:white), do: 9
+
+  @spec convert_with_unit(pos_integer()) :: {pos_integer(), :kiloohms | :ohms}
+  defp convert_with_unit(value) when value > 1000, do: {div(value, 1000), :kiloohms}
+  defp convert_with_unit(value), do: {value, :ohms}
 end
