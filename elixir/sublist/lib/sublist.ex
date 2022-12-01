a78cdfed8b7a4b5cf2ba843e5a45e31a0c77aa60
Seth Corker <seth.corker@gmail.com>
2022-12-01 12:20:32 +0000

    update sublist

--- elixir/sublist/lib/sublist.ex
+++ elixir/sublist/lib/sublist.ex
@@ -9,5 +9,23 @@ defmodule Sublist do
   def compare(_, []), do: :superlist
 
   def compare(a, b) do
+    a_count = Enum.count(a)
+    b_count = Enum.count(b)
+    b_chunk = Enum.take(b, a_count)
+
+    cond do
+      Enum.count(b_chunk) < b_count ->
+        :unequal
+
+      a_count !== b_count and a === b_chunk ->
+        :sublist
+
+      b_count === 1 and a !== b ->
+        :unequal
+
+      true ->
+        [_ | b_rest] = b
+        compare(a, b_rest)
+    end
   end
 end
